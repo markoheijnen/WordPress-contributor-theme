5d49a245e63a6717dc616dffb5c46c11cf4e0f89
Marko Heijnen <info@markoheijnen.com>
2014-10-18 14:26:17 +0200

    Display the retrieved information with Backbone.js

--- inc/theme-settings.php
+++ inc/theme-settings.php
@@ -24,8 +24,32 @@ class Contributor_Theme_Settings {
 			wp_die( __( 'You do not have sufficient permissions to manage options for this theme.', 'contributor' ) );
 		}
 
+		$template_blocks = array(
+			array(
+				'title' => __( 'Core contributions', 'contributor' ),
+				'list'  => 'core_contributions',
+				'count' => 'core_contributions_count',
+			),
+			array(
+				'title' => __( 'Codex contributions', 'contributor' ),
+				'list'  => 'codex_items',
+				'count' => 'codex_items_count',
+			),
+			array(
+				'title' => __( 'Plugins', 'contributor' ),
+				'list'  => 'plugins',
+			),
+			array(
+
+				'title' => __( 'Themes', 'contributor' ),
+				'list'  => 'themes',
+			),
+		);
+
+
 		wp_enqueue_style( 'contributor-admin',  get_template_directory_uri() . '/css/admin.css' );
-		wp_enqueue_script( 'contributor-admin',  get_template_directory_uri() . '/js/admin.js', array( 'jquery' ) );
+		wp_enqueue_script( 'contributor-admin',  get_template_directory_uri() . '/js/admin.js', array( 'jquery', 'wp-backbone' ) );
+		wp_localize_script( 'contributor-admin', 'contributor_template_blocks', $template_blocks );
 		?>
 
 		<div class="wrap">
@@ -37,24 +61,77 @@ class Contributor_Theme_Settings {
 					set_theme_mod( 'contributor_username', sanitize_text_field( $_POST['contributor_username'] ) );
 				}
 			}
+
+			$contributor_username = get_theme_mod('contributor_username');
+			$profile = new Contributor_Profile_Builder( $contributor_username );
+			$data = $profile->get_data();
 			?>
 
 			<br />
 
-			<form action="" method="post">
-				<div class="contributor-search">
+			<form id="contributor" action="" method="post">
+				<div class="contributor-search contributor-block">
 					<div class="contributor-search-input">
-						<input type="text" name="contributor_username" value="<?php echo get_theme_mod('contributor_username'); ?>" placeholder="<?php _e( 'What is your WordPress username?', 'contributor' ); ?>" size="30">
+						<input type="text" name="contributor_username" value="<?php echo $contributor_username; ?>" placeholder="<?php _e( 'What is your WordPress username?', 'contributor' ); ?>" size="30">
 					</div>
 
 					<button type="button" class="button-primary"><?php _e( 'Search', 'contributor' ); ?></button>
+				</div>
+
+				<div id="contributions">
+					<?php
+					if ( $contributor_username && $data ) {
+						foreach ( $template_blocks as $block ) {
+
+						}
+					}
+
+					?>
 				</div>
 
 				<?php submit_button(); ?>
 			</form>
 		</div>
 
+
+		<script type="text/template" id="tmpl-contributor_profile">
+			<?php echo $this->profile_template(); ?>
+		</script>
+
+		<script type="text/template" id="tmpl-contributor_block">
+			<?php echo $this->block_template(); ?>
+		</script>
+
 		<?php
+	}
+
+
+	private function profile_template() {
+		$html  = '<div class="contributor-profile contributor-block">';
+		$html .= '<# if ( data.avatar ) { #>';
+		$html .= '	<div class="profile-avatar">';
+		$html .= '		<img src="{{ data.avatar }}" alt="" />';
+		$html .= '	</div>';
+		$html .= '<# } #>';
+
+		$html .= '	<div class="contributor-info">';
+		$html .= '		<h2>{{ data.name }}</h2>';
+		$html .= '		<p>{{ data.company }}</p>';
+		$html .= '		<p><strong>' . __( 'Website', 'contributor' ) . ':</strong> <a href="{{ data.website }}">{{ data.website }}</a></p>';
+		$html .= '	</div>';
+
+		$html .= '</div>';
+
+		return $html;
+	}
+
+	private function block_template() {
+		$html  = '<div class="contributor-contribution contributor-block">';
+		$html .= '<h3>{{ data.title }}</h3>';
+
+		$html .= '</div>';
+
+		return $html;
 	}
 
 }
--- js/admin.js
+++ js/admin.js
@@ -1,19 +1,66 @@
 jQuery( document ).ready(function( $ ) {
 	var $contributor_search_button = $('.contributor-search button');
 
-	$contributor_search_button.css( 'border-color', $contributor_search_button.css( 'background-color' ) );
-
-	$contributor_search_button.on('hover blur', function() {
-		$contributor_search_button.css( 'border-color', $contributor_search_button.css( 'background-color' ) );
-	});
-
-	$contributor_search_button.on('click', function(evt) {
-		evt.preventDefault();
-
-		$.getJSON( 'http://wpcentral.io/api/contributors/' + $('.contributor-search input').val(), function( data ) {
-			console.log(data);
-		}).error(function(event, jqxhr, exception) {
-			console.log('oops');
-		});
-	});
+	var search_view = new ContributorView({ el: $("#contributor") });
 });
+
+( function($) {
+	ContributorView = wp.Backbone.View.extend({
+		username: false,
+		search_button: $('.contributor-search button'),
+		template: wp.template('contributor_profile'),
+		block_template: wp.template('contributor_block'),
+
+		initialize: function(){
+			var self = this;
+
+			self.search_button.css( 'border-color', self.search_button.css( 'background-color' ) );
+
+			self.search_button.on('mouseenter mouseleave blur', function() {
+				self.search_button.css( 'border-color', self.search_button.css( 'background-color' ) );
+			});
+		},
+		render: function() {
+			var self = this;
+			var options;
+
+			if ( this.prepare && this.username ) {
+				options = this.prepare();
+			}
+
+			this.views.detach();
+
+			if ( this.template ) {
+				options = options || {};
+				this.trigger( 'prepare', options );
+
+				var html = this.template( options );
+
+				$.each( contributor_template_blocks, function( key, block_info ) {
+					html += self.block_template( block_info );
+				});
+
+				this.$el.find('#contributions').html( html );
+			}
+
+			this.views.render();
+			return this;
+		},
+		events: {
+			"click .contributor-search button": "doSearch"  
+		},
+		doSearch: function( event ){
+			var self = this;
+
+			self.username = $('.contributor-search input').val();
+
+			$.getJSON( 'http://wpcentral.io/api/contributors/' + self.username, function( data ) {
+				self.options = data;
+				self.render();
+			}).error(function(event, jqxhr, exception) {
+				self.username = false;
+				self.render();
+			});
+		}
+	});
+})( jQuery );
