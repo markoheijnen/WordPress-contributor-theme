bec2d849a6a7657b827ba13f728192c9a3c70776
Marko Heijnen <info@markoheijnen.com>
2014-10-21 16:59:30 +0200

    Move footer part to it's own template

--- footer.php
+++ footer.php
@@ -10,13 +10,8 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'contributor' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'contributor' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'contributor' ), 'Contributor', '<a href="http://codekitchen.eu" rel="designer">CodeKitchen B.V.</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<?php get_template_part('parts/footer'); ?>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
---
+++ parts/footer.php
@@ -0,0 +1,10 @@
+
+	<footer id="colophon" class="site-footer block" role="contentinfo">
+		<div class="wrapper">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'contributor' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'contributor' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'contributor' ), 'Contributor', '<a href="http://codekitchen.eu" rel="designer">CodeKitchen B.V.</a>' ); ?>
+			</div><!-- .site-info -->
+		</div>
+	</footer><!-- #colophon -->
